d833f308adf49ecd63cc121ca547f2b4c193d63d
Nurudin Anwar Fauzi <72348799+anwarf86@users.noreply.github.com>
2022-06-10 10:04:13 +0700

    Create sewakan.js

---
+++ src/scripts/views/pages/sewakan.js
@@ -0,0 +1,13 @@
+const Sewakan = {
+  async render() {
+    return `
+      <h2>sewa Page</h2>
+    `;
+  },
+
+  async afterRender() {
+    // Fungsi ini akan dipanggil setelah render()
+  },
+};
+
+export default Sewakan;
