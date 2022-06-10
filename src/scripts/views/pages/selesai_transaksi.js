d037a39c6d9816586bd139f1f2bc70eb689efc11
Nurudin Anwar Fauzi <72348799+anwarf86@users.noreply.github.com>
2022-06-10 10:03:48 +0700

    Create selesai_transaksi.js

---
+++ src/scripts/views/pages/selesai_transaksi.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-tabs */
+const Selesai = {
+  async render() {
+    return `
+		<div class="container pt-3 pb-5">
+			<ol class="alur-pesan">
+				<li>1. Pilih Kendaraan  >  </li>
+				<li>2. Bayar  >  </li>
+				<li>3. Konfirmasi Order  >  </li>
+				<li class="onPage">4. Selesai</li>
+			</ol>
+			<hr>
+			<div class="card selesai mx-auto p-3">
+				<i class="material-icons text-center mx-auto">done</i>
+				<h3 class="pt-4">Bukti pembayaran Anda telah terkirim.</h3>
+				<p>Silahkan hubungi nomor WhatsApp pemilik untuk pengambilan kendaraan sewa.</p>
+				<div class="button-whatsapp">
+					<a href="">
+						<i class="fa fa-whatsapp p-2 text-center" style="font-size:20px">  Chat Via WhatsApp</i>
+					</a>
+				</div>
+			</div>
+		</div>
+		`;
+  },
+
+  async afterRender() {
+    // Fungsi ini akan dipanggil setelah render()
+  },
+};
+
+export default Selesai;
