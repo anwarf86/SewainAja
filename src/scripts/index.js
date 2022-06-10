2874bbe6ca66a0a86c2db8f87ff4bc15e18d1ac6
Nurudin Anwar Fauzi <72348799+anwarf86@users.noreply.github.com>
2022-06-10 09:09:30 +0700

    Create index.js

---
+++ src/scripts/index.js
@@ -0,0 +1,19 @@
+import 'regenerator-runtime';
+import '../styles/style.css';
+import '../styles/responsive.css';
+import App from './views/app';
+
+const app = new App({
+  button: document.querySelector('.navbar-toggler'),
+  drawer: document.querySelector('#navbarSupportedContent'),
+  content: document.querySelector('#mainContent'),
+  footer: document.querySelector('#footer'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+});
