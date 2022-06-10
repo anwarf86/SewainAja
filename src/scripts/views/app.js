3b76a866f2bc8267be55ada531eaca27b46e74ab
Nurudin Anwar Fauzi <72348799+anwarf86@users.noreply.github.com>
2022-06-10 09:08:53 +0700

    Create app.js

---
+++ src/scripts/views/app.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-underscore-dangle */
+import UrlParser from '../routes/url-parser';
+import routes from '../routes/routes';
+
+class App {
+  constructor({
+    button, drawer, content, footer,
+  }) {
+    this._button = button;
+    this._drawer = drawer;
+    this._content = content;
+    this._footer = footer;
+  }
+
+  async renderPage() {
+    const url = UrlParser.parseActiveUrlWithCombiner();
+    const page = routes[url];
+    this._content.innerHTML = await page.render();
+    await page.afterRender();
+  }
+}
+
+export default App;
